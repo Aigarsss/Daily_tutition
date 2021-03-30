7758793f78eecc9a259dd01a591d11b3be07f421
Aigarsss <aigars.uplejs@gmail.com>
2021-03-30 19:44:34 +0300

    finished carrusel

--- js/main.js
+++ js/main.js
@@ -18,17 +18,58 @@ window.addEventListener('scroll',(e)=>{
 
     navMenu.classList.toggle("show");
 
-    // if (navMenu.style.display === 'none' || navMenu.style.display == "") {
-    //     navMenu.style.display = 'block';
-    //     console.log("first");
-    // } else {
-    //     navMenu.style.display = 'none';
-    //     console.log("second");
-    // }
   }
 
 
 // carusel
-document.querySelector(".dots-container").addEventListener("click", e => {
-  console.log(e.target.children);
+
+// get crrusel container
+const carrusel = document.querySelector('.carrusel'); 
+// get the carrusel cards
+const carruselCards = Array.from(carrusel.children);
+// console.log(carruselCards);
+const dotsNav = document.querySelector('.dots-container');
+const dots = Array.from(dotsNav.children);
+// console.log(dots);
+const carruselWidth = carruselCards[0].getBoundingClientRect().width;
+
+
+const setCarruselPosition = (slide, index) => {
+  if (index > 0) {
+    slide.style.left = carruselWidth * index + 100 * index + 'px';
+  } else {
+    slide.style.left = carruselWidth * index + 'px';
+  }
+};
+
+carruselCards.forEach(setCarruselPosition);
+
+const moveToCard = (carrusel, currentCard, targetCard) => {
+  carrusel.style.transform = `translate(-${targetCard.style.left})`;
+  currentCard.classList.remove('card-active');
+  targetCard.classList.add('card-active');
+}
+
+
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove('card-active');
+  targetDot.classList.add('card-active')
+};
+
+//dots handiling
+dotsNav.addEventListener('click', e => {
+  // what did we click?
+
+  const targetDot = e.target.closest('.dot');
+
+  if (!targetDot) return;
+
+  const currentCard = carrusel.querySelector('.card-active');
+  const currentDot = dotsNav.querySelector('.card-active');
+  const targetIndex = dots.findIndex(dot => dot === targetDot);
+  const targetCard = carruselCards[targetIndex];
+
+  moveToCard(carrusel, currentCard, targetCard);
+  updateDots(currentDot, targetDot);
+
 })
